cc8645f31e1ff7db2f7e923df4486d76ecedb1af
clnelson <dumb.droid.dev@outlook.com>
2021-08-31 11:48:15 -0400

    Removed git_token from plain text

--- homemade/git-clnelson.sh
+++ homemade/git-clnelson.sh
@@ -1,4 +1,15 @@
-#!/usr/bin/env bash
+#!/data/data/com.termux/files/usr/bin/env bash
+git config --global ui.color "true"
 
 git config --global user.name "clnelson"
-git config --global user.email "dumb.droid.dev@outlook.com"
+git config --global user.email \
+	"dumb.droid.dev@outlook.com"
+
+# Git CLI no longer supports password
+# authentication
+git config --global remote.url \
+	"https://github.com"
+git config --global remote.token \
+	"$GIT_TOKEN"
+git config --global credential.helper \
+	"cache --timeout=7200"
