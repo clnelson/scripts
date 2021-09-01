e3476a06780417d362d84bea4e0d1d0d37af6218
clnelson <dumb.droid.dev@outlook.com>
2021-08-31 20:14:03 -0400

    Removed non-Termux scripts to seperate them into branches

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
-Then, run these commands
-
+Run these commands
 ```bash
 $ git clone https://github.com/clnelson/scripts
 $ cd scripts
 $ bash setup/<name of script>
 ```
-
-`android_build_env.sh` is for Ubuntu, Linux Mint & other distros using the `apt` package manager.
-The rest are named as per the distro.
-
-Please run the correct script depending on the distro you have installed!
-
 Enjoy!
 
 ### Brief explanation of stuff here
 
-setup -> setup Android Build Environment / Android SDK & various configurations.
-
-homemade -> my oh-my-bash/custom folder with all my settings and configs.
-
-working -> random stuff that I 'think' I'm going to get around to changing/modifying.
+setup -> setup Android Termux app with various configurations & automation.
--- setup/termux-setup.sh
+++ setup/termux-setup.sh
@@ -2,24 +2,24 @@
 
 ## Script for helping automate new Termux
 ## installations on Android devices
+
 pkg upgrade -y
 
 apt-get update && apt-get install \
-	science-repo game-repo unstable-repo x11-repo git -y
+	science-repo game-repo unstable-repo x11-repo -y
 
 apt-get install aapt aria2 bat binutils curl exa fd \
-	ffmpeg file geany geany-plugins gzip hexedit inxi lsof \
-	make man most nano neofetch nmap openjdk-17 openssh \
-	progress pv python python2 rsync tar texinfo unzip \
-	wget xz-utils zlib zstd -y
+	ffmpeg file geany geany-plugins git git-delta gzip \
+	hexedit inxi lsof make man most nano neofetch nmap \
+	openjdk-17 openssh progress pv python python2 rsync \
+	tar texinfo unzip wget2 xz-utils zlib zstd -y
 
-git clone https://github.com/Biggin/Termux_Bash -b \
-  mobile ~/termux_bash
+git clone https://github.com/Biggin/termux_bash -b mobile ~/
 
 if [ -d ~/termux_bash/bash ]; then
-	cd ~/termux_bash/bash || return 13
+	cd ~/termux_bash/bash || exit 13
 	cp ../.infokey ~/
-	cp -r .config .termux banner nanorc \
+	cp -r .config .local .termux banner nanorc \
 		.dircolors .git* ~/
 	cd ~/ && mv banner nanorc $PREFIX/etc/
 	echo -ne '\nneofetch\ncat $PREFIX/etc/banner' >> $PREFIX/etc/profile
@@ -33,3 +33,6 @@ if [ -d ~/termux_bash/bash ]; then
 		rm -rf ~/.dotfiles/custom
 	fi
 fi
+
+termux-reload-settings
+termux-setup-storage
