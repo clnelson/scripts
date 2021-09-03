a44d1c743692237daef5321cd74089b4dd43d4ed
clnelson <dumb.droid.dev@outlook.com>
2021-09-02 22:22:42 -0400

    Changed create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -2,10 +2,16 @@
 
 # Script to add a new Name to a Linux System
 export Name="${1}"
+
 if [ -z "${Name}" ]; then
-    echo -e "Please enter a Name"
+    echo -e ${Cya}"Please enter a Name"${Res}
     read -r Name
 fi
+sleep 1
+
 sudo useradd "${Name}" -m -s /bin/bash
 passwd "${Name}"
 chage -d 0 "${Name}"
+sleep 1
+
+echo -e ${Red}"You'll be required to change your password on your next login"${Res}
--- git-id.sh
+++ git-id.sh
@@ -13,7 +13,9 @@
  	GIT_USERNAME="$(git config --get user.name)"
  	GIT_EMAIL="$(git config --get user.email)"
 
-	echo -ne ${Bwhi}"Configuring git!\n\n"
+	echo -ne ${Bwhi}"Setting up git identity!\n\n"
+	sleep 2
+
  	if [[ -z ${GIT_USERNAME} ]]; then
  		echo -e ${Bcya}"Enter your git username: "
  		read -r NAME
@@ -28,10 +30,12 @@
  fi
 
 echo -e ${Grn}"Git identity setup successfully!"${Res}
-sleep 2
-echo -e ${Mag}"$(git config --list)"${Res}
+sleep 1
 
 ## Git configuration variables for my system
+echo -e ${Yel}"Configuring git settings..."${Res}
+sleep 2
+
 git config --global core.editor "nano"
 git congig --global branch.autoSetupRebase "remote"
 git config --global ui.color "true"
@@ -70,36 +74,38 @@ git config --global color.grep.selected "bold black"
 git config --global color.grep.separator "bold yellow"
 git config --global color.interactive "always"
 git config --global color.interactive.prompt "bold white"
-get config --global color.interactive.header "yellow"
-get config --global color.interactive.help "bold magenta"
-get config --global color.interactive.error "bold red"
-get config --global color.pager "enable"
-get config --global color.showBranch "always"
-get config --global color.status "always"
-get config --global color.status.header "bold white"
-get config --global color.status.added "green"
-get config --global color.status.changed "yellow"
-get config --global color.status.untracked "bold black"
-get config --global color.status.branch "cyan"
-get config --global color.status.nobranch "red"
-get config --global color.status.localBranch "white"
-get config --global color.status.remoteBranch "red"
-get config --global color.status.unmerged "magenta"
-get config --global color.ui "always"
-get config --global column.ui "always"
-get config --global column.status "always"
-get config --global column.tag "always"
-get config --global diff.algorithm "minimal"
-get config --global fetch.prune "true"
-get config --global fetch.output "compact"
-get config --global format.pretty "always"
-get config --global help.format "web"
-get config --global help.browser "firefox"
-get config --global merge.conflictStyle "diff3"
-get config --global merge.log "true"
-get config --global merge.tool "bc"
-get config --global rebase.stat "true"
-get config --global rebase.autoStash "true"
-get config --global rerere.autoUpdate "true"
+git config --global color.interactive.header "yellow"
+git config --global color.interactive.help "bold magenta"
+git config --global color.interactive.error "bold red"
+git config --global color.pager "enable"
+git config --global color.showBranch "always"
+git config --global color.status "always"
+git config --global color.status.header "bold white"
+git config --global color.status.added "green"
+git config --global color.status.changed "yellow"
+git config --global color.status.untracked "bold black"
+git config --global color.status.branch "cyan"
+git config --global color.status.nobranch "red"
+git config --global color.status.localBranch "white"
+git config --global color.status.remoteBranch "red"
+git config --global color.status.unmerged "magenta"
+git config --global color.ui "always"
+git config --global column.ui "always"
+git config --global column.status "always"
+git config --global column.tag "always"
+git config --global diff.algorithm "minimal"
+git config --global fetch.prune "true"
+git config --global fetch.output "compact"
+git config --global format.pretty "always"
+git config --global help.format "web"
+git config --global help.browser "firefox"
+git config --global merge.conflictStyle "diff3"
+git config --global merge.log "true"
+git config --global merge.tool "bc"
+git config --global rebase.stat "true"
+git config --global rebase.autoStash "true"
+git config --global rerere.autoUpdate "true"
 git config --global rerere.enabled "true"
 git config --global credential.helper "cache --timeout=7200"
+
+echo -e ${Bblu}"$(git config --list)"${Res}
